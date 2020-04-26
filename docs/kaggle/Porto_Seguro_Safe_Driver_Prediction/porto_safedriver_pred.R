d1c43db2fe0c5149a2558ef7d985f4f451f12e14
Unfinishedgod <shjj08@gmail.com>
2020-04-27 01:31:56 +0900

    porto safe driver pred

--- docs/kaggle/Porto_Seguro_Safe_Driver_Prediction/porto_safedriver_pred.R
+++ docs/kaggle/Porto_Seguro_Safe_Driver_Prediction/porto_safedriver_pred.R
@@ -9,6 +9,24 @@ library(aws.s3)
 
 # localH2O = h2o.init(nthreads=-1)
 
+# install h20
+
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+pkgs <- c("RCurl","jsonlite")
+for (pkg in pkgs) {
+  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+}
+
+install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/latest_stable_R")))
+
+library(h2o)
+localH2O = h2o.init()
+demo(h2o.kmeans)
+
+####
+
 h2o.init()
 # getwd()
 
