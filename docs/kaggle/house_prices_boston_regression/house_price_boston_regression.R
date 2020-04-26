91ca2f74afb0f4faf3d0cfec84418b0b07704fb3
Unfinishedgod <shjj08@gmail.com>
2020-04-27 00:49:28 +0900

    porto

--- docs/kaggle/house_prices_boston_regression/house_price_boston_regression.R
+++ docs/kaggle/house_prices_boston_regression/house_price_boston_regression.R
@@ -169,3 +169,69 @@ which(colnames(all) == "SalePrice")
 
 colSums(is.na(all))
 all$SalePrice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+require(h2o)
+require(tidyverse)
+require(data.table)
+require(skimr)
+
+df <- fread('../input/creditcard.csv')
+
+df <- iris
+
+df$Class <- as.factor(df$Class)
+
+# Clustered Time...it might be useful
+clusts <- kmeans(df$Time, centers = 4)
+df$time2 <- clusts$cluster
+df <- df %>% select(-Time)
+
+df <- df %>% select(time2, everything())
+
+y <- 'Species'
+
+h2o.init()
+
+df <- as.h2o(df)
+
+split <- h2o.splitFrame(df, 0.7, seed=1)
+train <- split[[1]]
+test <- split[[2]]
+
+h2o.table(train$Species)
+h2o.table(test$Species)
+
+
+aml <- h2o.automl(y=y, training_frame = train, seed=1, nfolds = 5, balance_classes = T,
+                  max_runtime_secs = 600)
+aml
+#AUC is high at 0.9739
+
+pred <- h2o.predict(aml, newdata = test)
+
+perf <- h2o.performance(aml@leader, newdata = test)
+perf
