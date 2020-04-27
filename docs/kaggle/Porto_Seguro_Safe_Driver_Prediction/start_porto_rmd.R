875d2ffad1e54602a35f291932cc91e6d8116ce9
Unfinishedgod <shjj08@gmail.com>
2020-04-27 17:51:40 +0900

    modified

--- docs/kaggle/Porto_Seguro_Safe_Driver_Prediction/start_porto_rmd.R
+++ docs/kaggle/Porto_Seguro_Safe_Driver_Prediction/start_porto_rmd.R
@@ -7,3 +7,5 @@ html_link <- "porto_safedriver_pred.html"
 render("/home/owen/Unfinishedgod.github.io/docs/kaggle/Porto_Seguro_Safe_Driver_Prediction/porto_safedriver_pred.Rmd",
        output_file = html_link,
        output_dir = "/home/owen/Unfinishedgod.github.io/docs/kaggle/Porto_Seguro_Safe_Driver_Prediction")
+
+
